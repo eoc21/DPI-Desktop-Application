6151fafe76242e36deb419c8672a720d67179485
Ed Cannon <eoc21@cam.ac.uk>
2010-02-25 11:33:27 +0000

    Added a class to represent a SPARQL query results object. Information on property type, its value, technique, condition and units used.

---
+++ src/dpi/DPISPARQLResult.java
@@ -0,0 +1,61 @@
+package dpi;
+
+public class DPISPARQLResult {
+	private String property;
+	private double propertyValue;
+	private String technique;
+	private String condition;
+	private String unit;
+	
+	public DPISPARQLResult(){
+	}
+	
+	public DPISPARQLResult(String propertyName, double propertyVal,String techniqueName,String conditionValue,String unitValue){
+		this.property = propertyName;
+		this.propertyValue = propertyVal;
+		this.technique = techniqueName;
+		this.condition = conditionValue;
+		this.unit = unitValue;
+	}
+	
+	public String getPropertyName(){
+		return property;
+	}
+	
+	public double getPropertyValue(){
+		return propertyValue;
+	}
+	
+	public String getTechnique(){
+		return technique;
+	}
+	
+	public String getCondition(){
+		return condition;
+	}
+	
+	public String getUnit(){
+		return unit;
+	}
+	
+	public void setPropertyName(String propertyName){
+		this.property = propertyName;
+	}
+	
+	public void setPropertyValue(double value){
+		this.propertyValue = value;
+	}
+	
+	public void setTechnique(String techniqueValue){
+		this.technique = techniqueValue;
+	}
+	
+	public void setCondition(String conditionValue){
+		this.condition = conditionValue;
+	}
+	
+	public void setUnit(String unitValue){
+		this.unit = unitValue;
+	}
+	
+}
--- src/dpi/ResultsFrame.java
+++ src/dpi/ResultsFrame.java
@@ -2,6 +2,7 @@ package dpi;
 
 import java.awt.FlowLayout;
 import java.io.IOException;
+import java.util.Vector;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -9,10 +10,16 @@ import javax.swing.JTable;
 
 import nu.xom.Builder;
 import nu.xom.Document;
+import nu.xom.Element;
+import nu.xom.Node;
+import nu.xom.Nodes;
 import nu.xom.ParsingException;
 import nu.xom.ValidityException;
 
 import com.hp.hpl.jena.query.ResultSet;
+
+import dpi.DPISPARQLResult;
+
 /**
  * Class to represent the SPARQL results in a table in a new frame. 
  * @author ed
@@ -58,10 +65,71 @@ public class ResultsFrame extends JFrame {
 	public  JTable getQueryResultsTable(ResultSet results){
 		return null;
 	}
-	
-	private void readInData() throws ValidityException, ParsingException, IOException{
+	/**
+	 * Method to read in SPARQL XML results file.
+	 * @return XOM Document
+	 * @throws ValidityException
+	 * @throws ParsingException
+	 * @throws IOException
+	 */
+	private Document readInData() throws ValidityException, ParsingException, IOException{
 		Builder builder = new Builder();
         Document doc = builder.build("ResultOutput.xml");
-        
+        return doc;
 	}
+	/**
+	 * Method to extract JTable column titles.
+	 * @param doc - XOM Document to extract headers.
+	 * @return Vector of header information.
+	 */
+	private Vector<String> getJTableColumnHeaders(Document doc){
+		Element root = doc.getRootElement();
+		Node headerInformation = root.getChild(1);
+		Vector<String> columnTitles = new Vector<String>();
+		for(int i=0;i<headerInformation.getChildCount();i++){
+			if( i%2  !=0){
+				columnTitles.add(headerInformation.getChild(i).query("attribute::name").get(0).getValue());
+			}
+		}
+		return columnTitles;
+	}
+	
+	private Vector<Vector<DPISPARQLResult>> getJTableData(Document doc){
+		Element root = doc.getRootElement();
+		Node childThree = root.getChild(3);
+		Vector<Vector<DPISPARQLResult>> individualRows = new Vector<Vector<DPISPARQLResult>>();
+		int counter = 0;
+		for(int i=0;i<childThree.getChildCount();i++){
+			if(i%2 !=0){
+				Nodes nd = childThree.getChild(i).query("child::*");
+				DPISPARQLResult sparqlHit = new DPISPARQLResult();
+				for(int j=0;j<nd.size();j++){
+					Nodes v = nd.get(j).query("@name");
+					if(v.get(0).getValue().toString().equals("x")){
+						String[] uriValue = nd.get(j).getValue().split("#");
+						sparqlHit.setPropertyName(uriValue[1]);
+					}
+					else if(v.get(0).getValue().toString().equals("hasValue")){
+						sparqlHit.setPropertyValue(Double.parseDouble(nd.get(j).getValue()));
+					}
+					else if(v.get(0).getValue().toString().equals("condition")){
+						sparqlHit.setCondition(nd.get(j).getValue());
+					}
+					else if(v.get(0).getValue().toString().equals("technique")){
+						sparqlHit.setTechnique(nd.get(j).getValue());
+					}
+					else if(v.get(0).getValue().toString().equals("hasUnit")){
+						sparqlHit.setUnit(nd.get(j).getValue());
+					}
+				}
+				Vector<DPISPARQLResult> aVector = new Vector<DPISPARQLResult>();
+				aVector.add(sparqlHit);
+				individualRows.add(counter, aVector);
+				counter++;
+			}
+		}
+		System.out.println(individualRows.get(0).get(0).getPropertyName());
+		return individualRows;
+	}
+
 }
